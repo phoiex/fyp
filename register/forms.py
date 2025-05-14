903418f7cd42dc71108156f9eb4cb4ed894992a3
Lai Xiangpeng <2633466174@qq.com>
2025-05-15 02:14:11 +0800

    reduce some redundant code, ensure atomic when user register

--- register/forms.py
+++ register/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from register.models import Company as Comp
-from register.models import UserProfile
+from django.db import transaction
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from register.models import Company as Comp, UserProfile
+
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(label='E-mail', required=True)
@@ -10,7 +11,7 @@ class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = {
+        fields = [
             'username',
             'first_name',
             'last_name',
@@ -18,43 +19,44 @@ class RegistrationForm(UserCreationForm):
             'company',
             'password1',
             'password2',
-        }
-
+        ]
         labels = {
             'first_name': 'Name',
             'last_name': 'Last Name',
             'company': 'Company',
         }
 
+    @transaction.atomic
     def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.username = self.cleaned_data['username']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
+        user = super().save(commit=False)
         user.email = self.cleaned_data['email']
-        company = self.cleaned_data['company']
 
         if commit:
             user.save()
-            user_profile = UserProfile.objects.create(user=user, company=Comp.objects.get(name=company))
-            user_profile.save()
+
+            company = self.cleaned_data['company']
+            # Get Company object from the database in a safe way
+            company_obj = Comp.objects.filter(name=company.name).first()
+
+            UserProfile.objects.create(user=user, company=company_obj)
 
         return user
 
     def __init__(self, *args, **kwargs):
-        super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['username'].widget.attrs['placeholder'] = 'Username'
-        self.fields['first_name'].widget.attrs['class'] = 'form-control'
-        self.fields['first_name'].widget.attrs['placeholder'] = 'First name'
-        self.fields['last_name'].widget.attrs['class'] = 'form-control'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Last name'
-        self.fields['email'].widget.attrs['class'] = 'form-control'
-        self.fields['email'].widget.attrs['placeholder'] = 'E-mail'
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['placeholder'] = 'Retype Password'
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password1': 'Password',
+            'password2': 'Retype Password',
+        }
+        for field, text in placeholders.items():
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': text
+            })
         self.fields['company'].widget.attrs['class'] = 'form-control'
 
 
@@ -68,7 +70,6 @@ class CompanyRegistrationForm(forms.Form):
     class Meta:
         model = Comp
 
-
     def save(self, commit=True):
         company = Comp()
         company.social_name = self.cleaned_data['social_name']
@@ -79,38 +80,42 @@ class CompanyRegistrationForm(forms.Form):
 
         if commit:
             company.save()
-
+        return company
 
     def __init__(self, *args, **kwargs):
-        super(CompanyRegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['social_name'].widget.attrs['class'] = 'form-control'
-        self.fields['social_name'].widget.attrs['placeholder'] = 'Social Name'
-        self.fields['name'].widget.attrs['class'] = 'form-control'
-        self.fields['name'].widget.attrs['placeholder'] = 'Name'
-        self.fields['email'].widget.attrs['class'] = 'form-control'
-        self.fields['email'].widget.attrs['placeholder'] = 'Email'
-        self.fields['city'].widget.attrs['class'] = 'form-control'
-        self.fields['city'].widget.attrs['placeholder'] = 'City'
-        self.fields['found_date'].widget.attrs['class'] = 'form-control'
-        self.fields['found_date'].widget.attrs['placeholder'] = 'Found date'
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'social_name': 'Social Name',
+            'name': 'Name',
+            'email': 'Email',
+            'city': 'City',
+            'found_date': 'Found date',
+        }
+        for field, text in placeholders.items():
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control',
+                'placeholder': text
+            })
 
 
 class ProfilePictureForm(forms.Form):
     img = forms.ImageField()
+
     class Meta:
         model = UserProfile
         fields = ['img']
 
     def save(self, request, commit=True):
-        user = request.user.userprofile_set.first()
-        user.img = self.cleaned_data['img']
+        user_profile = request.user.userprofile
+        user_profile.img = self.cleaned_data['img']
 
         if commit:
-            user.save()
-
-        return user
+            user_profile.save()
+        return user_profile
 
     def __init__(self, *args, **kwargs):
-        super(ProfilePictureForm, self).__init__(*args, **kwargs)
-        self.fields['img'].widget.attrs['class'] = 'custom-file-input'
-        self.fields['img'].widget.attrs['id'] = 'validatedCustomFile'
+        super().__init__(*args, **kwargs)
+        self.fields['img'].widget.attrs.update({
+            'class': 'custom-file-input',
+            'id': 'validatedCustomFile'
+        })
